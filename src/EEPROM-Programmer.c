91667cf46ec7cad6a43903e9f0b840527f212b8c
Chad Paradis <cparadis6191@gmail.com>
2013-11-30 22:41:30 -0500

    Added a simple skeleton framework for the program
    Read from UART/USB and write to EEPROM.

--- src/EEPROM-Programmer.c
+++ src/EEPROM-Programmer.c
@@ -1,15 +1,29 @@
 #include <avr/io.h>
+#include <stdlib.h>
+
 
 #include "EEPROM-Programmer.h"
 #include "eeprom.h"
 
 
 int main(void) {
+	uint32_t prog[255] = {};
+	size_t f_size;
+
 	eeprom_init();
 
 
-	while(1) {
-	
+	while (1) {
+		// Read in file
+		// Use either UART or USB
+
+		// Find file length
+		f_size = sizeof(prog);
+
+
+		for (uint16_t i = 0; i < 255; i++) {
+			eeprom_write_word(i, prog[i]);
+		}
 	}
 
 
