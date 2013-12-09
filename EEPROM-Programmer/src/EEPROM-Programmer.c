c6368f6fe18efcdd946e93a0f540790265c8877d
Chad Paradis <cparadis6191@gmail.com>
2013-12-09 18:55:15 -0500

    Added code to initialize I/O and wait for a button press

--- EEPROM-Programmer/src/EEPROM-Programmer.c
+++ EEPROM-Programmer/src/EEPROM-Programmer.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <avr/io.h>
 
 #include "EEPROM-Programmer.h"
 #include "eeprom.h"
 #include "data.h"
+#include "io.h"
 
 
 int main(void) {
 	size_t f_size;
 	uint8_t byte;
 
+	io_init();
 	eeprom_init();
 
 
@@ -18,13 +21,13 @@ int main(void) {
 	// data resides in program space instead of main memory
 	f_size = sizeof(data);
 
+	wait_for_press();
 	for (size_t i = 0; i < f_size; i++) {
 		// Read the byte in from program space
 		byte = pgm_read_byte(&(data[0]));
 
 		eeprom_write_word(i, byte);
 	}
-
 	while (1) {}
 
 
