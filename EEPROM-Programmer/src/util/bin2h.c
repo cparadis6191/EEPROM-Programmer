07df7c0c6c3174aa0eb2af7b0e1c4af9fc076cb4
Chad Paradis <cparadis6191@gmail.com>
2013-12-09 23:38:44 -0500

    Modified bin2h utility to take an opcode byte argument
    Pass 1, 2, or 3 to send the low, middle, or high byte of the opcode to the header file.
    This is used for programming the low, middle, or high byte EEPROM chips.

--- EEPROM-Programmer/src/util/bin2h.c
+++ EEPROM-Programmer/src/util/bin2h.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <stdint.h>
 
@@ -7,26 +8,37 @@
 
 int main(int argc, char **argv) {
 	int byte;
+	int byte_n = 0;
+	int byte_hml;
 
 	FILE *bin;
 	FILE *data_h;
-	const char *data_h_name = "../data.h";
+	const char *data_name = "../data.h";
 
 
 	// Make sure we only have one input file
-	if (argc != 2) {
-		perror("Please specify a file name...\n");
+	if (argc != 3) {
+		printf("Please specify a file name and opcode byte (321)\n");
+		return 1;
 	}
 
-	bin = fopen(argv[1], "r");
+	byte_hml = strtol(argv[2], NULL, 10);
+	if (byte_hml == 0L || byte_hml < 1 || byte_hml > 3) {
+		printf("Opcode section must be 321 corresponding to hml\n");
+		return 1;
+	}
+
+	// Open the input file
+	bin = fopen(argv[1], "rb");
 	if (bin == NULL) {
-		perror("Error opening input file...\n");
+		perror("Error opening input file");
 		return errno;
 	}
 
-	data_h = fopen(data_h_name, "w");
+	// Open the output file
+	data_h = fopen(data_name, "w");
 	if (data_h == NULL) {
-		perror("Error opening output file...\n");
+		perror("Error opening output file");
 		return errno;
 	}
 
@@ -42,16 +54,18 @@ int main(int argc, char **argv) {
 	fprintf(data_h, "// Reference for writing data to program space flash\n");
 	fprintf(data_h, "const uint8_t data[] PROGMEM = {\n");
 
-
-// http://www.nongnu.org/avr-libc/user-manual/pgmspace.html
-// Reference for writing data to program space flash
-
-
+	// Get and write bytes until we hit the end of the binary
+	// Only write specified opcode byte
 	while ((byte = fgetc(bin)) != EOF) {
-		fprintf(data_h, "\t0x%x,\n", byte);
+		if (byte_n%3 == byte_hml - 1) { 
+
+			fprintf(data_h, "\t0x%x,\n", byte);
+		}
+		++byte_n;
 	}
 
 	// Close the header information
+	fprintf(data_h, "\t0x0\n");
 	fprintf(data_h, "};\n");
 	fprintf(data_h, "\n\n");
 	fprintf(data_h, "#endif\n");
