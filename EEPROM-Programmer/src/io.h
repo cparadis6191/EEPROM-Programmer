e6e1ce8fddd34e5c6c0138257dc4a903a3c99b48
Chad Paradis <cparadis6191@gmail.com>
2013-12-11 00:25:03 -0500

    Added an led toggle macro and stdbool include

--- EEPROM-Programmer/src/io.h
+++ EEPROM-Programmer/src/io.h
@@ -1,8 +1,11 @@
 #ifndef __IO_H_
 #define __IO_H_
 
+#include <stdbool.h>
+
 #define F_CPU 2000000UL
 #include <util/delay.h>
+
 
 // Button is 3.3V when pressed
 #define BTNPORT PORTR
@@ -16,6 +19,7 @@
 #define LEDPIN PIN0_bm
 #define LED(VAL) if (VAL) { LEDPORT.OUTCLR = LEDPIN; \
                  } else     LEDPORT.OUTSET = LEDPIN
+#define LED_TGL() LEDPORT.OUTTGL = LEDPIN
 
 void io_init(void);
 
